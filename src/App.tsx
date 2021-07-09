14d63b872f4ec98d6e96a94c4f5f63d3e840fe03
GRID Lua Pele <gridluapele@GRIDs-MacBook-Pro.local>
2021-07-09 11:14:19 -1000

    pushing upstream

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,287 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { FinalScore } from "./utils/utils";
 
-function App() {
+const App: React.FC = () => {
+  const [state, setState] = useState({
+    ageInput: 0,
+    pushUpInput: 0,
+    sitUpInput: 0,
+    runInput: "",
+    gender: "male",
+    cantWalk: false,
+    cantRun: false,
+    cantPushUp: false,
+    cantSitUp: false,
+    finalScore: 0,
+  });
+  interface State {
+    ageInput: number;
+    pushUpInput: number;
+    sitUpInput: number;
+    runInput: string;
+    gender: string;
+    cantWalk: boolean;
+    cantRun: boolean;
+    cantPushUp: boolean;
+    cantSitUp: boolean;
+    finalScore: number;
+  }
+
+  const handleScoring = (scoreValues: typeof state) => {
+    const pushupInput = scoreValues.pushUpInput;
+    const situpInput = scoreValues.sitUpInput;
+
+    const cantPushUp = scoreValues.cantPushUp;
+    const cantSitUp = scoreValues.cantSitUp;
+    const cantRun = scoreValues.cantRun;
+
+    const newScore = FinalScore(
+      pushupInput,
+      cantPushUp,
+      situpInput,
+      cantSitUp,
+      0,
+      true
+    );
+    setState({
+      ...state,
+      finalScore: newScore,
+    });
+  };
+  useEffect(() => {
+    handleScoring(state);
+  }, [
+    state.ageInput,
+    state.pushUpInput,
+    state.sitUpInput,
+    state.runInput,
+    state.cantPushUp,
+    state.cantSitUp,
+    state.cantRun,
+  ]);
+  const handleFormInputChange = (value: any, field: string) => {
+    setState({
+      ...state,
+      [field]: value,
+    });
+  };
+
+  const handleRunInputMask = (value: string): number => {
+    return 0;
+  };
+
+  const ProfileForm = ({ state }: { state: State }) => {
+    return (
+      <div className="d-flex">
+        <div style={{ paddingRight: "1rem" }}>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="flexCheckDefault"
+              value={state.cantWalk ? "on" : "off"}
+              onChange={() =>
+                handleFormInputChange(!state.cantWalk, "cantWalk")
+              }
+            />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Walk
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="flexCheckChecked"
+              value={state.cantRun ? "on" : "off"}
+              onChange={() => handleFormInputChange(!state.cantRun, "cantRun")}
+            />
+            <label className="form-check-label" htmlFor="flexCheckChecked">
+              Run
+            </label>
+          </div>
+        </div>
+        <div style={{ paddingRight: "1rem" }}>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="flexCheckChecked"
+              value={state.cantPushUp ? "on" : "off"}
+              onChange={() =>
+                handleFormInputChange(!state.cantPushUp, "cantPushUp")
+              }
+            />
+            <label className="form-check-label" htmlFor="flexCheckChecked">
+              Push ups
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="flexCheckChecked"
+              value={state.cantSitUp ? "on" : "off"}
+              onChange={() =>
+                handleFormInputChange(!state.cantSitUp, "cantSitUp")
+              }
+            />
+            <label className="form-check-label" htmlFor="flexCheckChecked">
+              Sit Ups
+            </label>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid p-0">
+      {/*Header*/}
+      <div
+        className="col d-flex text-white p-3"
+        style={{ backgroundColor: "#026CA7" }}
+      >
+        <div className="row w-100">
+          <i className="bi bi-list h1 p-0 m-0 mx-3 " />
+        </div>
+        <div className="row w-100 text-center justify-content-center align-content-center fw-bold">
+          PT Calculator
+        </div>
+        {/*Blank Row*/}
+        <div className="row w-100" />
+      </div>
+      {/*Score input area*/}
+      <div className="col d-flex px-5 py-3">
+        <div className="w-75">
+          <div className="pb-2 fw-bold">Scores</div>
+          <div className="form-floating mb-3">
+            <input
+              type="number"
+              className="form-control"
+              id="ageInput"
+              placeholder="Age"
+              value={state.ageInput}
+              onChange={(event) =>
+                handleFormInputChange(parseInt(event.target.value), "ageInput")
+              }
+            />
+            <label htmlFor="floatingInput">Age</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="number"
+              className="form-control"
+              id="pushUpInput"
+              placeholder="Push ups"
+              value={state.pushUpInput}
+              onChange={(event) =>
+                handleFormInputChange(
+                  parseInt(event.target.value),
+                  "pushUpInput"
+                )
+              }
+            />
+            <label htmlFor="floatingInput">Push ups</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="number"
+              className="form-control"
+              id="sitUpInput"
+              placeholder="Sit ups"
+              value={state.sitUpInput}
+              onChange={(event) =>
+                handleFormInputChange(
+                  parseInt(event.target.value),
+                  "sitUpInput"
+                )
+              }
+            />
+            <label htmlFor="floatingPassword">Sit ups</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="text"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Run Time"
+              value={state.runInput}
+              onChange={(event) =>
+                handleFormInputChange(
+                  handleRunInputMask(event.target.value),
+                  "runInput"
+                )
+              }
+            />
+            <label htmlFor="floatingPassword">Run Time</label>
+          </div>
+        </div>
+        <div className="row w-25" />
+      </div>
+      {/*Gender Options*/}
+      <div className="col d-flex px-5 py-3">
+        <div className="w-75">
+          <div className="pb-2 fw-bold">Gender</div>
+          <div className="d-flex">
+            <div className="form-check mx-3">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="genderInputMale"
+                value={state.gender == "male" ? "on" : "off"}
+                onChange={() => handleFormInputChange("male", "gender")}
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault1">
+                Male
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="genderInputFemale"
+                value={state.gender == "female" ? "on" : "off"}
+                onChange={() => handleFormInputChange("female", "gender")}
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault2">
+                Female
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="row w-25" />
+      </div>
+      {/*Profile Options*/}
+      <div className="col d-flex px-5 py-3">
+        <div className="w-75">
+          <div className="pb-2 fw-bold">Profiles</div>
+          <ProfileForm state={state} />
+        </div>
+        <div className="row w-25" />
+      </div>
+      <div className="px-5 py-3">
+        <div className="fw-bold">Results</div>
+        <div>
+          Score: {Math.floor(state.finalScore * 10) / 10}
+          /100
+        </div>
+        <div className="progress">
+          <div
+            className="progress-bar progress-bar-striped bg-success"
+            role="progressbar"
+            style={{ width: `${state.finalScore}%` }}
+            // ariaValuenow="25"
+            // ariaValuemin="0"
+            // ariaValuemax="100"
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utils/utils.ts
@@ -0,0 +1,182 @@
+import _ from "lodash";
+
+const ageGetter = (ageInput: number): string => {
+  const ageRangeMap = [
+    { ageArray: [17, 25], ageRangeString: "17-24" },
+    { ageArray: [25, 30], ageRangeString: "25-29" },
+    { ageArray: [30, 35], ageRangeString: "30-34" },
+    { ageArray: [35, 40], ageRangeString: "35-39" },
+    { ageArray: [40, 45], ageRangeString: "40-44" },
+    { ageArray: [45, 50], ageRangeString: "45-49" },
+    { ageArray: [50, 55], ageRangeString: "50-54" },
+    { ageArray: [55, 60], ageRangeString: "55-59" },
+    { ageArray: [60, 100], ageRangeString: "60+" },
+  ];
+
+  const ageRangeObj = ageRangeMap.find((obj) =>
+    _.inRange(ageInput, obj.ageArray[0], obj.ageArray[1])
+  );
+
+  return ageRangeObj ? ageRangeObj.ageRangeString : "";
+};
+
+const pushUpsGetter = (pushUpsInput: number, pushUpWaiver: boolean): number => {
+  let pushUpsScore;
+
+  const pushUpCountToScore = [
+    { count: 30, score: 1 },
+    { count: 31, score: 4.0 },
+    { count: 32, score: 7.0 },
+    { count: 33, score: 10.0 },
+    { count: 34, score: 10.6 },
+    { count: 35, score: 11.0 },
+    { count: 36, score: 11.6 },
+    { count: 37, score: 12.0 },
+    { count: 38, score: 12.6 },
+    { count: 39, score: 13.0 },
+    { count: 40, score: 13.6 },
+    { count: 41, score: 14.0 },
+    { count: 42, score: 14.4 },
+    { count: 43, score: 14.6 },
+    { count: 44, score: 15.0 },
+    { count: 45, score: 15.4 },
+    { count: 46, score: 15.6 },
+    { count: 47, score: 16.0 },
+    { count: 48, score: 16.2 },
+    { count: 49, score: 16.6 },
+    { count: 50, score: 16.8 },
+    { count: 51, score: 17.0 },
+    { count: 52, score: 17.2 },
+    { count: 53, score: 17.4 },
+    { count: 54, score: 17.6 },
+    { count: 55, score: 17.7 },
+    { count: 56, score: 17.8 },
+    { count: 57, score: 18.0 },
+    { count: 58, score: 18.2 },
+    { count: 59, score: 18.4 },
+    { count: 60, score: 18.6 },
+    { count: 61, score: 18.8 },
+    { count: 62, score: 19.0 },
+    { count: 63, score: 19.2 },
+    { count: 64, score: 19.4 },
+    { count: 65, score: 19.6 },
+    { count: 66, score: 19.8 },
+    { count: 67, score: 20.0 },
+  ];
+
+  if (
+    pushUpsInput < 30 ||
+    pushUpsInput === undefined ||
+    pushUpsInput == null ||
+    pushUpWaiver
+  ) {
+    pushUpsScore = 0;
+  } else if (pushUpsInput >= 68) {
+    pushUpsScore = 20;
+  } else {
+    const pushUpsScoreObj = pushUpCountToScore.find(
+      (obj) => obj.count === pushUpsInput
+    );
+    pushUpsScore = pushUpsScoreObj ? pushUpsScoreObj.score : 0;
+  }
+
+  return pushUpsScore;
+};
+
+const sitUpsGetter = (sitUpInput: number, SitUpWaiver: boolean): number => {
+  let sitUpsScore;
+  if (SitUpWaiver) {
+    return 0;
+  }
+  const sitUpCountToScore = [
+    { count: 39, score: 3.0 },
+    { count: 40, score: 6.0 },
+    { count: 41, score: 9.0 },
+    { count: 42, score: 12.0 },
+    { count: 43, score: 12.6 },
+    { count: 44, score: 13.0 },
+    { count: 45, score: 14.0 },
+    { count: 46, score: 15.0 },
+    { count: 47, score: 16.0 },
+    { count: 48, score: 16.6 },
+    { count: 49, score: 17.0 },
+    { count: 50, score: 17.4 },
+    { count: 51, score: 17.6 },
+    { count: 52, score: 18.0 },
+    { count: 53, score: 18.4 },
+    { count: 54, score: 18.8 },
+    { count: 55, score: 19.0 },
+    { count: 56, score: 19.4 },
+    { count: 57, score: 19.7 },
+    { count: 58, score: 20.0 },
+  ];
+  if (
+    sitUpInput < 39 ||
+    sitUpInput === undefined ||
+    sitUpInput == null ||
+    SitUpWaiver
+  ) {
+    sitUpsScore = 0;
+  } else if (sitUpInput >= 58) {
+    sitUpsScore = 20;
+  } else {
+    const sitUpScoreObj = sitUpCountToScore.find(
+      (obj) => obj.count === sitUpInput
+    );
+    sitUpsScore = sitUpScoreObj ? sitUpScoreObj.score : 0;
+  }
+  return sitUpsScore;
+};
+
+const RunTimeGetter = (RunTime: number, RunWaiver: boolean): number => {
+  let runTimeScore = 0;
+  if (RunWaiver) {
+    runTimeScore = 0.0;
+  }
+  if (RunTime > 1) {
+    runTimeScore = 0;
+  }
+  if (RunTime === 2) {
+    runTimeScore = 60.0;
+  }
+  return runTimeScore;
+};
+
+const FinalScore = (
+  pushUpInput: number,
+  pushUpWaiver: boolean,
+  sitUpInput: number,
+  sitUpWaiver: boolean,
+  runTimeInput: number,
+  runWaiver: boolean
+): number => {
+  const firstScore = pushUpsGetter(pushUpInput, pushUpWaiver);
+  const secondScore = firstScore + sitUpsGetter(sitUpInput, sitUpWaiver);
+  let totalScore = secondScore + RunTimeGetter(runTimeInput, runWaiver);
+
+  if (pushUpWaiver && sitUpWaiver && runWaiver) {
+    totalScore = 100;
+  } else if (pushUpWaiver && sitUpWaiver) {
+    totalScore = (totalScore / 60) * 100;
+  } else if (pushUpWaiver && runWaiver) {
+    totalScore = (totalScore / 20) * 100;
+  } else if (runWaiver && sitUpWaiver) {
+    totalScore = (totalScore / 20) * 100;
+  } else if (runWaiver) {
+    totalScore = (totalScore / 40) * 100;
+  } else if (pushUpWaiver) {
+    totalScore = (totalScore / 80) * 100;
+  } else if (sitUpWaiver) {
+    totalScore = (totalScore / 80) * 100;
+  }
+
+  return totalScore;
+};
+
+export {
+  ageGetter,
+  pushUpsGetter,
+  sitUpsGetter as SitUpsGetter,
+  RunTimeGetter,
+  FinalScore,
+};
